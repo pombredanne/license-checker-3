13fdf06b0a1587f8562be3e62a8a4dbba5a14c0e
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2014-09-06 13:36:10 +0800

    Change test directory to tests

--- test.py
+++ test.py
@@ -2,5 +2,5 @@
 import unittest
 
 if __name__ == '__main__':
-    testsuite = unittest.TestLoader().discover('test')
+    testsuite = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=1).run(testsuite)
