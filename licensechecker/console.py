1119bdce5e97975e978c3aa649d72e396af07271
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2014-09-06 12:54:56 +0800

    Parse command line argument

--- licensechecker/console.py
+++ licensechecker/console.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
+import argparse
+
+import licensechecker
+
+def get_args():
+    parser = argparse.ArgumentParser(description=licensechecker.__description__)
+    return parser.parse_args()
+
 
 def main():
-    print('license-checker')
+    args = get_args()
+
 
 if __name__ == '__main__':
     main()
